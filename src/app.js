5954803e3ed49827644697d5cfb1bcdecaa1128c
dhrxxuv <Dhruvneekhra01@gmail.com>
2025-05-17 02:15:19 +0530

    original

--- src/app.js
+++ src/app.js
@@ -33,8 +33,8 @@ app.use('/', userRouter);
 connectDB()
     .then(() => {
         console.log('DB connected');
-        app.listen(7777, () => {
-            console.log('Server is running on port 7777');
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
         });
     })
     .catch((err) => {
